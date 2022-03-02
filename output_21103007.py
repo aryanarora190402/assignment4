a79e078f9ca434ea8f5cc652c71f19ccd0529df9
Aryan Arora <aryanarora190402@gmail.com>
2022-03-02 11:32:04 +0530

    Create output_21103007.py

---
+++ output_21103007.py
@@ -0,0 +1,64 @@
+Q1
+Move disk 1 from rod A to rod C
+Move disk 2 from rod A to rod B
+Move disk 1 from rod C to rod B
+Move disk 3 from rod A to rod C
+Move disk 1 from rod B to rod A
+Move disk 2 from rod B to rod C
+Move disk 1 from rod A to rod C
+
+Q2
+Enter the number of rows: 5
+Recursion:
+1
+1 1
+1 2 1
+1 3 3 1
+1 4 6 4 1
+Iteration:
+1
+
+1 1
+
+1 2 1
+
+1 3 3 1
+
+1 4 6 4 1
+
+Q3
+Enter value of a and b: 5 2
+(Quotient, Remainder): (2, 1)
+
+3a)
+Function is callable: False
+3b)
+Values entered are non-zero: Yes
+3c)
+After addition: (2, 1, 4, 5, 6) and after filtering: (2, 1, 4)
+3d)
+{1, 2, 4}
+3e)
+frozenset({1, 2, 4})
+3f)
+Max value is:  4 and its Hash value is: 4
+
+Q4
+Enter name of the student: Aryan
+Enter SID of the student: 3007
+Name:  Aryan
+SID:  3007
+The object has been destroyed.
+
+Q5
+5a)
+Updated Salary of Mehak:  70000
+5b)
+Viren's record deleted.
+
+Q6
+Q6
+Enter a word: aryan
+Enter another word: arora
+The possible words are:  ['aryan', 'rayan', 'ryaan', 'ryaan', 'ryana']
+The friendship is fake
